2b6d749cf688a4c18cbddc35b51c819f291e553d
Gustavo Ferraz <gferraz754@gmail.com>
2025-10-01 13:01:54 -0300

    Update aula09.py

---
+++ aula09.py
@@ -0,0 +1,54 @@
+# frase = 'Curso em vídeo Python'
+# print(frase[9::3])
+
+# frase = 'Curso em vídeo Python'
+# print(len(frase))
+
+# frase = 'Curso em vídeo Python'
+# print(frase.count('o'))
+
+# frase = 'Curso em vídeo Python'
+# print(frase.count('o', 0, 13))
+
+# frase = 'Curso em vídeo Python'
+# print(frase.find('deo'))
+
+# frase = 'Curso em vídeo Python'
+# print(frase.find('Android'))
+
+# frase = 'Curso em vídeo Python'
+# print('Curso' in frase)
+
+# frase = 'Curso em vídeo Python'
+# print(frase.replace('Python', 'Android'))
+
+#frase = 'Curso em vídeo Python'
+#print(frase.upper())
+
+#frase = 'Curso em vídeo Python'
+#print(frase.lower())
+
+# frase = 'Curso em vídeo Python'
+# print(frase.capitalize())
+
+# frase = 'Curso em vídeo Python'
+# print(frase.title())
+
+# frase = '   Aprendo Python  '
+# print(frase.strip())
+
+# frase = '   Aprendo Python  '
+# print(frase.rstrip())
+
+# frase = '   Aprendo Python  '
+# print(frase.lstrip())
+
+# frase = 'Curso em Vídeo Python'
+# print(frase.split())
+
+# frase = 'Curso em Vídeo Python  '
+# print('-'.join(frase.split()))
+
+# frase = 'Curso em Vídeo Python  '
+# print('-'.join(frase))
+
