7c1b1ba0a17ea54d8066638ae847d0505251f5d6
Gustavo Ferraz <gferraz754@gmail.com>
2025-10-01 16:24:29 -0300

    Update desafio022.py

--- desafio022.py
+++ desafio022.py
@@ -8,6 +8,7 @@ print('Seu nome minúsculas é: {}'.format(nome.lower()))
 print('Seu mome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
 
 #print('Seu nome tem ao todo {} letras'.format(nome.find(' ')))
+#Gustavo
 
 separa = nome.split()
 print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
