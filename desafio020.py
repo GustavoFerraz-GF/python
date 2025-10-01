135fc2381619a14e0ece2a35b30ba1e31cfc6603
Gustavo Ferraz <gferraz754@gmail.com>
2025-10-01 11:03:33 -0300

    Update desafio020.py

--- desafio020.py
+++ desafio020.py
@@ -5,5 +5,5 @@ n3 = str(input("Terceiro aluno: "))
 n4 = str(input("Quarto aluno: "))
 lista = [n1, n2, n3, n4]
 shuffle(lista)
-print("A ordem de apresentação será {}")
+print("A ordem de apresentação será: ")
 print(lista)
