bb4e9b8faddf0b587bafa74eb220d92865d5575d
Gustavo Ferraz <gferraz754@gmail.com>
2025-10-08 16:27:27 -0300

    Desafio 29 e 30.

--- desafio029.py
+++ desafio029.py
@@ -1,1 +1,10 @@
 velocidade = int(input('Qual a velocidade atual do carro? '))
+limite = 80
+multa_por_km = 7
+km_acima = velocidade - limite
+pagar = km_acima * multa_por_km
+if velocidade > limite:
+    print('MULTADO! Você excedeu o limite permitido que é de 80km/h')
+    print('Você deve pagar uma multa de R$ {:.2f}'.format(pagar))
+   
+print('Tenha um bom dia! Dirija com segurança!')
