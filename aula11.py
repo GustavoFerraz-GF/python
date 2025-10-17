2bfde9c93bae5a877f02f57facc5bf687450da42
Gustavo Ferraz <gferraz754@gmail.com>
2025-10-17 19:25:18 -0300

    Update aula11.py

--- aula11.py
+++ aula11.py
@@ -1,1 +1,10 @@
-# Cores no Terminal.
+'''a = 3
+b = 5
+print(f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m!!!')'''
+
+nome = 'Gustavo'
+cores = {'limpa':'\033[m',
+        'azul':'\033[34m', 
+        'amarelo':'\033[33m', 
+        'pretoebranco':'\033[7;30m'}
+print(f'Olá! Muito prazer em te conhecer, {cores['azul']}{nome}{cores['limpa']}')
